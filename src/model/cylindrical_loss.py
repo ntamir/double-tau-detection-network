50cc6c008579bcf41d1eb60904961fbf6cdccfe0
Idan Stark <idanstark42@gmail.com>
2024-10-18 23:48:37 +0300

    not using position in order to keep the gradient working

--- src/model/cylindrical_loss.py
+++ src/model/cylindrical_loss.py
@@ -1,16 +1,16 @@
-# an MSE loss with one of the dimensions is a circle and the other is a line
-
 import torch
-
-from data.position import Position
 
 class CylindricalLoss (torch.nn.Module):
   def __init__(self):
     super(CylindricalLoss, self).__init__()
 
-  def forward(self, output, target):
-    target_positions = [Position(tau[0], tau[1]) for tau in target.view(-1, 2)]
-    output_positions = [Position(tau[0], tau[1]) for tau in output.view(-1, 2)]
-    distances = [target_position.distance(output_position) for target_position, output_position in zip(target_positions, output_positions)]
-    distances = torch.tensor(distances, requires_grad=True)
-    return torch.mean(distances)
+    def forward(self, output, target):
+        output = output.view(-1, 2)
+        target = target.view(-1, 2)
+
+        circular_diff = torch.remainder(output[:, 0] - target[:, 0] + torch.pi, 2 * torch.pi) - torch.pi
+        linear_diff = output[:, 1] - target[:, 1]
+
+        distances = torch.sqrt(circular_diff ** 2 + linear_diff ** 2)
+
+        return torch.mean(distances)

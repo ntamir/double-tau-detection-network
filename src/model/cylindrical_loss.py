797eeeaef468057ff638924e073a01cb4b96c550
Idan Stark <idanstark42@gmail.com>
2024-10-18 23:50:01 +0300

    bugfix

--- src/model/cylindrical_loss.py
+++ src/model/cylindrical_loss.py
@@ -4,13 +4,13 @@ class CylindricalLoss (torch.nn.Module):
   def __init__(self):
     super(CylindricalLoss, self).__init__()
 
-    def forward(self, output, target):
-        output = output.view(-1, 2)
-        target = target.view(-1, 2)
+  def forward(self, output, target):
+    output = output.view(-1, 2)
+    target = target.view(-1, 2)
 
-        circular_diff = torch.remainder(output[:, 0] - target[:, 0] + torch.pi, 2 * torch.pi) - torch.pi
-        linear_diff = output[:, 1] - target[:, 1]
+    circular_diff = torch.remainder(output[:, 0] - target[:, 0] + torch.pi, 2 * torch.pi) - torch.pi
+    linear_diff = output[:, 1] - target[:, 1]
 
-        distances = torch.sqrt(circular_diff ** 2 + linear_diff ** 2)
+    distances = torch.sqrt(circular_diff ** 2 + linear_diff ** 2)
 
-        return torch.mean(distances)
+    return torch.mean(distances)

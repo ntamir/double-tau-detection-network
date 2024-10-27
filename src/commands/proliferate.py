d876b16647fd1a01ef599d1cffcf9d1fb64c5bbb
Idan Stark <idanstark42@gmail.com>
2024-10-27 16:06:37 +0200

    trying faster proliferation

--- src/commands/proliferate.py
+++ src/commands/proliferate.py
@@ -6,6 +6,8 @@ from visualization import DatasetVisualizer
 
 def proliferate (dataset, factor):
   initial_count = len(dataset)
+  flips = 0
+  rotations = []
 
   def run (next):
     new_data = {
@@ -14,13 +16,12 @@ def proliferate (dataset, factor):
       'tracks': [None] * len(dataset) * factor,
       'truthTaus': [None] * len(dataset) * factor
     }
-    flips = 0
-    rotations = []
 
     def get (index):
       return { key: dataset.raw_data[key][index] for key in dataset.raw_data }
   
     def generate_copies (original):
+      nonlocal flips, rotations
       copies = [{ key: np.copy(original[key]) for key in original } for _ in range(factor)]
       for copy in copies:
         # if random > 0.5 is odd, flip eta
@@ -39,19 +40,15 @@ def proliferate (dataset, factor):
     for i in range(len(dataset)):
       original = get(i)
       copies = generate_copies(original)
-      new_data['event'][i*factor] = original['event']
-      new_data['clusters'][i*factor] = original['clusters']
-      new_data['tracks'][i*factor] = original['tracks']
-      new_data['truthTaus'][i*factor] = original['truthTaus']
 
-      new_data['event'][i*factor+1:(i+1)*factor] = [copy['event'] for copy in copies]
-      new_data['clusters'][i*factor+1:(i+1)*factor] = [copy['clusters'] for copy in copies]
-      new_data['tracks'][i*factor+1:(i+1)*factor] = [copy['tracks'] for copy in copies]
-      new_data['truthTaus'][i*factor+1:(i+1)*factor] = [copy['truthTaus'] for copy in copies]
-    return new_data, flips, rotations
+      for key in dataset.raw_data:
+        new_data[key][i*factor] = original[key]
+        new_data[key][i*factor+1:(i+1)*factor] = [copy[key] for copy in copies]
+
+    return new_data
 
   print('Starting proliferation...')
-  dataset.raw_data, flips, rotations = long_operation(run, max=len(dataset), message='Proliferating')
+  dataset.raw_data = long_operation(run, max=len(dataset), message='Proliferating')
   print()
   print('Done.')
   print(f'Proliferated {initial_count} events by factor of {factor} to {len(dataset)}')
@@ -60,14 +57,14 @@ def proliferate (dataset, factor):
   DatasetVisualizer(dataset).show_proliferation(flips, rotations)
 
 def flip (event):
-  event['Clusters.calEta'] = -event['Clusters.calEta']
-  event['Tracks.eta'] = -event['Tracks.eta']
-  event['TruthTaus.eta_vis'] = -event['TruthTaus.eta_vis']
+  event['clusters']['Clusters.calEta'] = -event['clusters']['Clusters.calEta']
+  event['tracks']['Tracks.eta'] = -event['tracks']['Tracks.eta']
+  event['truthTaus']['TruthTaus.eta_vis'] = -event['truthTaus']['TruthTaus.eta_vis']
 
 def rotate (event, by):
-  event['Clusters.calPhi'] = rotate_angles(event['Clusters.calPhi'], by)
-  event['Tracks.phi'] = rotate_angles(event['Tracks.phi'], by)
-  event['TruthTaus.phi_vis'] = rotate_angles(event['TruthTaus.phi_vis'], by)
+  event['clusters']['Clusters.calPhi'] = rotate_angles(event['clusters']['Clusters.calPhi'], by)
+  event['tracks']['Tracks.phi'] = rotate_angles(event['tracks']['Tracks.phi'], by)
+  event['truthTaus']['TruthTaus.phi_vis'] = rotate_angles(event['truthTaus']['TruthTaus.phi_vis'], by)
 
 
 def rotate_angles(angles, by):

997441b1be98edff367a55602a976153898761c8
Idan Stark <idanstark42@gmail.com>
2024-10-27 12:40:31 +0200

    better merging

--- src/commands/merge.py
+++ src/commands/merge.py
@@ -2,16 +2,51 @@
 
 import h5py
 
+def create_output_file (output_file, input_file):
+  with h5py.File(input_file, 'r') as input:
+    tracks = input['tracks'][:]
+    cells = input['clusters'][:]
+    truthTaus = input['truthTaus'][:]
+    with h5py.File(output_file, 'w') as output:
+      output.create_dataset(
+        "tracks",
+        data=tracks,
+        compression="gzip",
+        chunks=True,
+        maxshape=(None, tracks.shape[1]),
+      )
+      output.create_dataset(
+        "clusters",
+        data=cells,
+        compression="gzip",
+        chunks=True,
+        maxshape=(None, cells.shape[1]),
+      )
+      output.create_dataset(
+        "truthTaus",
+        data=truthTaus,
+        compression="gzip",
+        chunks=True,
+        maxshape=(None, cells.shape[1]),
+      )
+
+def append_to_output_file (output_file, input_file):
+  with h5py.File(input_file, 'r') as input:
+    tracks = input['tracks'][:]
+    cells = input['clusters'][:]
+    truthTaus = input['truthTaus'][:]
+    with h5py.File(output_file, 'a') as output:
+      output['tracks'].resize((output['tracks'].shape[0] + tracks.shape[0]), axis=0)
+      output['tracks'][-tracks.shape[0]:] = tracks
+      output['clusters'].resize((output['clusters'].shape[0] + cells.shape[0]), axis=0)
+      output['clusters'][-cells.shape[0]:] = cells
+      output['truthTaus'].resize((output['truthTaus'].shape[0] + truthTaus.shape[0]), axis=0)
+      output['truthTaus'][-truthTaus.shape[0]:] = truthTaus
+
 def merge_h5_files(input_files, output_file):
-  with h5py.File(output_file, 'w') as output_h5:
-    for input_file in input_files:
-      with h5py.File(input_file, 'r') as input_h5:
-        for key in input_h5.keys():
-          if key in output_h5:
-            output_h5[key].resize(output_h5[key].shape[0] + input_h5[key].shape[0], axis=0)
-            output_h5[key][-input_h5[key].shape[0]:] = input_h5[key][:]
-          else:
-            output_h5.create_dataset(key, data=input_h5[key][:])
+  create_output_file(output_file, input_files[0])
+  for input_file in input_files[1:]:
+    append_to_output_file(output_file, input_file)
 
 if __name__ == '__main__':
   input_files = [

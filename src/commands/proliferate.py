8b168d38af85513ddc5323fcd711d55b3a24c930
Idan Stark <idanstark42@gmail.com>
2024-10-27 15:49:02 +0200

    trying faster proliferation

--- src/commands/proliferate.py
+++ src/commands/proliferate.py
@@ -8,41 +8,46 @@ def proliferate (dataset, factor):
   initial_count = len(dataset)
 
   def run (next):
-    new_data = { 'event': [], 'clusters': [], 'tracks': [], 'truthTaus': [] }
-
+    new_data = {
+      'event': [None] * len(dataset) * factor,
+      'clusters': [None] * len(dataset) * factor,
+      'tracks': [None] * len(dataset) * factor,
+      'truthTaus': [None] * len(dataset) * factor
+    }
     flips = 0
     rotations = []
 
     def get (index):
       return { key: dataset.raw_data[key][index] for key in dataset.raw_data }
-    
-    def add (event):
-      for key in new_data:
-        new_data[key].append(event[key])
-
-    for index in range(len(dataset)):
-      original = get(index)
-      add(original)
-      for i in range(factor - 1):
-        # deep copy of the event
-        copy = { key: np.copy(original[key]) for key in original }
+  
+    def generate_copies (original):
+      copies = [{ key: np.copy(original[key]) for key in original } for _ in range(factor)]
+      for copy in copies:
         # if random > 0.5 is odd, flip eta
         if np.random.rand() > 0.5:
           flips += 1
-          copy['clusters']['Clusters.calEta'] = -copy['clusters']['Clusters.calEta']
-          copy['tracks']['Tracks.eta'] = -copy['tracks']['Tracks.eta']
-          copy['truthTaus']['TruthTaus.eta_vis'] = -copy['truthTaus']['TruthTaus.eta_vis']
+          flip(copy)
 
         # rotate phi by a random angle
         random_angle = np.random.rand() * (PHI_RANGE[1] - PHI_RANGE[0])
         rotations.append(random_angle)
-          
-        copy['clusters']['Clusters.calPhi'] = rotate(copy['clusters']['Clusters.calPhi'], random_angle)
-        copy['tracks']['Tracks.phi'] = rotate(copy['tracks']['Tracks.phi'], random_angle)
-        copy['truthTaus']['TruthTaus.phi_vis'] = rotate(copy['truthTaus']['TruthTaus.phi_vis'], random_angle)
-
-        add(copy)
+        rotate(copy, random_angle)
+      
       next()
+      return copies
+
+    for i in range(len(dataset)):
+      original = get(i)
+      copies = generate_copies(original)
+      new_data['event'][i*factor] = original['event']
+      new_data['clusters'][i*factor] = original['clusters']
+      new_data['tracks'][i*factor] = original['tracks']
+      new_data['truthTaus'][i*factor] = original['truthTaus']
+
+      new_data['event'][i*factor+1:(i+1)*factor] = [copy['event'] for copy in copies]
+      new_data['clusters'][i*factor+1:(i+1)*factor] = [copy['clusters'] for copy in copies]
+      new_data['tracks'][i*factor+1:(i+1)*factor] = [copy['tracks'] for copy in copies]
+      new_data['truthTaus'][i*factor+1:(i+1)*factor] = [copy['truthTaus'] for copy in copies]
     return new_data, flips, rotations
 
   print('Starting proliferation...')
@@ -54,5 +59,16 @@ def proliferate (dataset, factor):
 
   DatasetVisualizer(dataset).show_proliferation(flips, rotations)
 
-def rotate(angles, by):
+def flip (event):
+  event['Clusters.calEta'] = -event['Clusters.calEta']
+  event['Tracks.eta'] = -event['Tracks.eta']
+  event['TruthTaus.eta_vis'] = -event['TruthTaus.eta_vis']
+
+def rotate (event, by):
+  event['Clusters.calPhi'] = rotate_angles(event['Clusters.calPhi'], by)
+  event['Tracks.phi'] = rotate_angles(event['Tracks.phi'], by)
+  event['TruthTaus.phi_vis'] = rotate_angles(event['TruthTaus.phi_vis'], by)
+
+
+def rotate_angles(angles, by):
   return (angles + by) % (PHI_RANGE[1] - PHI_RANGE[0]) + PHI_RANGE[0]

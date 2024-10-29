eab6e5594870025533b22e47a1922df69ac33886
ntamir <nadavtamir@mail.tau.ac.il>
2024-10-29 23:30:10 +0200

    Update dataset.py

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -41,7 +41,7 @@ class EventsDataset (Dataset):
     if self.use_cache:
       self.cache[index] = item
     cache_time = time()
-    print(f'Load: {load_time - start:.2f}s, Item: {item_time - load_time:.2f}s, Cache: {cache_time - item_time:.2f}s')
+    #print(f'Load: {load_time - start:.2f}s, Item: {item_time - load_time:.2f}s, Cache: {cache_time - item_time:.2f}s')
     return item
 
   def __getitem__(self, index):
